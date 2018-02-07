6086a1fc284ed394b3905e7f20752dd76cd66a42
Vargese Antony <Vargese.antony@hotmail.com>
2018-02-07 10:33:20 -0600

    Adding store-devtools to the project

--- src/app/company/company.module.ts
+++ src/app/company/company.module.ts
@@ -5,6 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 // Ngrx imports
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { companyReducers } from '../state-management/reducers/company.reducer';
 import { CompanyEffects } from '../state-management/effects/company.effects';
 
@@ -23,7 +25,8 @@ import { CompanyDetailResolveService } from './company-add/company-detail-resolv
     ReactiveFormsModule,
     CompanyRoutingModule,
     StoreModule.forRoot(companyReducers),
-    EffectsModule.forRoot([CompanyEffects])
+    EffectsModule.forRoot([CompanyEffects]),
+    StoreDevtoolsModule.instrument()
   ],
   declarations: [
     CompanyListComponent,
